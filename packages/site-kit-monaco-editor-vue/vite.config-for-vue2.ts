3dfee23e15b3749d2cde86adf59cca7715807f6e
yosuke ota <otameshiyo23@gmail.com>
2022-11-20 16:14:51 +0900

    feat: pre-build vue component

---
+++ packages/site-kit-monaco-editor-vue/vite.config-for-vue2.ts
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { vue2Plugin } from "../site-kit-vite-plugin-vue2/vite-config-builder.js";
+import bundleCssPlugin from "./vite-plugins/bundle-css.js";
+
+export default defineConfig({
+  plugins: [vue2Plugin(), bundleCssPlugin({ fileName: "MonacoEditor.vue2" })],
+  build: {
+    outDir: __dirname,
+    cssCodeSplit: false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "MonacoEditor.vue"),
+      name: "MonacoEditor",
+      // the proper extensions will be added
+      fileName: "MonacoEditor.vue2",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue", "@ota-meshi/site-kit-monaco-editor"],
+    },
+  },
+});
