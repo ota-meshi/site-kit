4b263542da518fb4510747067fea344b45abf6e9
yosuke ota <otameshiyo23@gmail.com>
2024-07-12 14:01:42 +0900

    fix: syntax highlighting issue

--- packages/site-kit-monarch-syntaxes/src/utils/register-language.ts
+++ packages/site-kit-monarch-syntaxes/src/utils/register-language.ts
@@ -17,22 +17,32 @@ export function registerLanguage(
 ): void {
   const { language, loadLang, loadConfig } = def;
   const languageId = language.id;
+  const models = monaco.editor
+    .getModels()
+    .filter((model) => model.getLanguageId() === languageId);
+
   monaco.languages.register(language);
-  monaco.languages.registerTokensProviderFactory(languageId, {
-    create: loadLang,
-  });
-  monaco.languages.onLanguageEncountered(languageId, async () => {
-    const conf = await loadConfig();
-    monaco.languages.setLanguageConfiguration(languageId, conf);
-  });
-  if (
-    monaco.editor
-      .getModels()
-      .some((model) => model.getLanguageId() === languageId)
-  ) {
-    monaco.languages.setMonarchTokensProvider(languageId, loadLang());
-    Promise.resolve(loadConfig()).then((conf) => {
+
+  if (!models.length) {
+    monaco.languages.registerTokensProviderFactory(languageId, {
+      create: loadLang,
+    });
+    monaco.languages.onLanguageEncountered(languageId, async () => {
+      const conf = await loadConfig();
       monaco.languages.setLanguageConfiguration(languageId, conf);
     });
+  } else {
+    const lang = loadLang();
+    const config = Promise.resolve(loadConfig());
+    monaco.languages.setMonarchTokensProvider(languageId, lang);
+    Promise.all([lang, config])
+      .then(([, conf]) => {
+        monaco.languages.setLanguageConfiguration(languageId, conf);
+      })
+      .then(() => {
+        for (const model of models) {
+          monaco.editor.setModelLanguage(model, languageId);
+        }
+      });
   }
 }
